a0619b9d75b6b1f97da0c3f57eeafe4e0a7efbde
wangqinghai <471259444@qq.com>
2018-11-05 19:44:04 +0800

    第二次修改

--- git_text/src/com/itheima/gitText.java
+++ git_text/src/com/itheima/gitText.java
@@ -4,5 +4,6 @@ public class gitText {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("修改了一次");
+        System.out.println("克隆成功，修改");
     }
 }
